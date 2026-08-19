001364f0dcaa989032b8b89ccad658e05e8be6d0
hhhhhooo-2333 <1398981086@qq.com>
2026-08-19 15:27:06 +0800

    更新

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='pets3-review-v2';
-const CORE=['./','./index.html','./styles.css','./extras.css','./app.js','./words.js','./manifest.webmanifest','./icon.svg'];
+const CORE=['./','./index.html','./styles.css','./extras.css','./records.css','./app.js','./words.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
